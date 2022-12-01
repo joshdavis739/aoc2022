fb0b889e0d5711500ddff0662897476ce6f871af
Josh Davis <josh.davis@enable.com>
2022-12-01 18:54:57 +0000

    Add day 1.1

---
+++ UI/Program.cs
@@ -0,0 +1,20 @@
+﻿var input = File.ReadAllLines("C:/Projects/Advent of Code 2022/UI/Day1.txt");
+var current = 0;
+var largest = 0;
+foreach (var line in input)
+{
+    if (line.Length == 0)
+    {
+        if (current > largest)
+        {
+            largest = current;
+        }
+
+        current = 0;
+    }
+    else
+    {
+        current += int.Parse(line);
+    }
+}
+Console.WriteLine(largest);
