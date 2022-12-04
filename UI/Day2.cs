ef556666197e0bb7297adfd912491a574aa3dee5
Josh Davis <josh.davis@enable.com>
2022-12-04 14:27:26 +0000

    Add (a rather gross) day 2 part 2

--- UI/Day2.cs
+++ UI/Day2.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UI
+﻿namespace UI
 {
     internal class Day2
     {
         internal IDictionary<char, Thrown> _opponentThrowSymbols = new Dictionary<char, Thrown>();
-        internal IDictionary<char, Thrown> _playerThrowSymbols = new Dictionary<char, Thrown>();
+        internal IDictionary<char, State> _stateSymbols = new Dictionary<char, State>();
         internal IDictionary<Thrown, int> _thrownScores = new Dictionary<Thrown, int>();
         internal IDictionary<State, int> _stateScores = new Dictionary<State, int>();
 
@@ -27,9 +21,9 @@ namespace UI
             _opponentThrowSymbols.Add('B', Thrown.Paper);
             _opponentThrowSymbols.Add('C', Thrown.Scissors);
 
-            _playerThrowSymbols.Add('X', Thrown.Rock);
-            _playerThrowSymbols.Add('Y', Thrown.Paper);
-            _playerThrowSymbols.Add('Z', Thrown.Scissors);
+            _stateSymbols.Add('X', State.Loss);
+            _stateSymbols.Add('Y', State.Draw);
+            _stateSymbols.Add('Z', State.Win);
         }
 
         internal int RunDay2_1()
@@ -52,29 +46,47 @@ namespace UI
             }
 
             var opponentThrow = _opponentThrowSymbols[row[0]];
-            var playerThrow = _playerThrowSymbols[row[2]];
+            var desiredState = _stateSymbols[row[2]];
 
-            var state = StateForThrows(opponentThrow, playerThrow);
+            var playerThrow = ThrowForGivenOpponentThrowAndState(opponentThrow, desiredState);
             var scoreForThrow = _thrownScores[playerThrow];
-            var scoreForState = _stateScores[state];
+            var scoreForState = _stateScores[desiredState];
             return scoreForThrow + scoreForState;
         }
 
-        private State StateForThrows(Thrown opponentThrow, Thrown playerThrow)
+        private Thrown ThrowForGivenOpponentThrowAndState(Thrown opponentThrow, State state)
         {
-            if (opponentThrow == playerThrow)
+            if (state == State.Draw)
             {
-                return State.Draw;
+                return opponentThrow;
             }
 
-            if ((opponentThrow == Thrown.Rock && playerThrow == Thrown.Paper)
-                || (opponentThrow == Thrown.Paper && playerThrow == Thrown.Scissors)
-                || (opponentThrow == Thrown.Scissors && playerThrow == Thrown.Rock))
+            if (state == State.Win)
             {
-                return State.Win;
+                if (opponentThrow == Thrown.Rock)
+                {
+                    return Thrown.Paper;
+                }
+                if (opponentThrow == Thrown.Scissors)
+                {
+                    return Thrown.Rock;
+                }
+                if (opponentThrow == Thrown.Paper)
+                {
+                    return Thrown.Scissors;
+                }
             }
 
-            return State.Loss;
+            if (opponentThrow == Thrown.Rock)
+            {
+                return Thrown.Scissors;
+            }
+            if (opponentThrow == Thrown.Scissors)
+            {
+                return Thrown.Paper;
+            }
+            
+            return Thrown.Rock;
         }
     }
 
