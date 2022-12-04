580c837f5663158b594230a5b7e07e70ad247f49
Josh Davis <josh.davis@enable.com>
2022-12-04 13:46:30 +0000

    Add day 2 part 1

---
+++ UI/Day2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    internal class Day2
+    {
+        internal IDictionary<char, Thrown> _opponentThrowSymbols = new Dictionary<char, Thrown>();
+        internal IDictionary<char, Thrown> _playerThrowSymbols = new Dictionary<char, Thrown>();
+        internal IDictionary<Thrown, int> _thrownScores = new Dictionary<Thrown, int>();
+        internal IDictionary<State, int> _stateScores = new Dictionary<State, int>();
+
+        internal Day2()
+        {
+            _thrownScores.Add(Thrown.Rock, 1);
+            _thrownScores.Add(Thrown.Paper, 2);
+            _thrownScores.Add(Thrown.Scissors, 3);
+
+            _stateScores.Add(State.Loss, 0);
+            _stateScores.Add(State.Draw, 3);
+            _stateScores.Add(State.Win, 6);
+
+            _opponentThrowSymbols.Add('A', Thrown.Rock);
+            _opponentThrowSymbols.Add('B', Thrown.Paper);
+            _opponentThrowSymbols.Add('C', Thrown.Scissors);
+
+            _playerThrowSymbols.Add('X', Thrown.Rock);
+            _playerThrowSymbols.Add('Y', Thrown.Paper);
+            _playerThrowSymbols.Add('Z', Thrown.Scissors);
+        }
+
+        internal int RunDay2_1()
+        {
+            var input = File.ReadAllLines("C:/Projects/Advent of Code 2022/UI/Day2.txt");
+            var score = 0;
+            foreach (var row in input)
+            {
+                score += ParseScoreForRow(row);
+            }
+
+            return score;
+        }
+
+        private int ParseScoreForRow(string row)
+        {
+            if (row.Length != 3)
+            {
+                throw new Exception();
+            }
+
+            var opponentThrow = _opponentThrowSymbols[row[0]];
+            var playerThrow = _playerThrowSymbols[row[2]];
+
+            var state = StateForThrows(opponentThrow, playerThrow);
+            var scoreForThrow = _thrownScores[playerThrow];
+            var scoreForState = _stateScores[state];
+            return scoreForThrow + scoreForState;
+        }
+
+        private State StateForThrows(Thrown opponentThrow, Thrown playerThrow)
+        {
+            if (opponentThrow == playerThrow)
+            {
+                return State.Draw;
+            }
+
+            if ((opponentThrow == Thrown.Rock && playerThrow == Thrown.Paper)
+                || (opponentThrow == Thrown.Paper && playerThrow == Thrown.Scissors)
+                || (opponentThrow == Thrown.Scissors && playerThrow == Thrown.Rock))
+            {
+                return State.Win;
+            }
+
+            return State.Loss;
+        }
+    }
+
+    internal enum Thrown
+    {
+        Rock = 0,
+        Paper = 1,
+        Scissors = 2
+    }
+
+    internal enum State
+    {
+        Loss = 0,
+        Draw = 1,
+        Win = 2
+    }
+}
--- UI/Program.cs
+++ UI/Program.cs
@@ -1,34 +1,6 @@
-﻿var input = File.ReadAllLines("C:/Projects/Advent of Code 2022/UI/Day1.txt");
-var current = 0;
-var numberOfItemsToTrack = 3;
-var largestItems = Array.Empty<int>();
+﻿using UI;
 
-foreach (var line in input)
-{
-    if (line.Length == 0)
-    {
-        if (largestItems.Length < numberOfItemsToTrack)
-        {
-            largestItems = largestItems.Append(current).OrderBy(x => x).ToArray();
-        }
-        else if (current > largestItems[0])
-        {
-            var newLargestItems = largestItems.Skip(1).Append(current).OrderBy(x => x).ToArray();
-            largestItems = newLargestItems;
-        }
+var day2 = new Day2();
+var output = day2.RunDay2_1();
 
-        current = 0;
-    }
-    else
-    {
-        current += int.Parse(line);
-    }
-}
-
-if (current > largestItems[0])
-{
-    var newLargestItems = largestItems.Skip(1).Append(current).OrderBy(x => x).ToArray();
-    largestItems = newLargestItems;
-}
-
-Console.WriteLine(largestItems.Sum());
+Console.WriteLine(output);
