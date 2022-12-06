4f4df787e348c5344bc128f1dbcf82b19216f7e0
Josh Davis <josh.davis@enable.com>
2022-12-06 12:32:13 +0000

    Hack in d3p1

---
+++ UI/Day3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    internal class Day3
+    {
+        public int RunPart1()
+        {
+            var value = 0;
+            var input = File.ReadAllLines("C:/Projects/Advent of Code 2022/UI/Day3.txt");
+            foreach (var line in input)
+            {
+                var leftCompartment = line.Take(line.Length / 2).Select(x => (int)(x))
+                    .Select(x => (x >= 97 && x <= 122) ? x - 96 : x - 38);
+                var rightCompartment = line.Skip(line.Length / 2).Take(line.Length / 2).Select(x => (int)(x))
+                    .Select(x => (x >= 97 && x <= 122) ? x - 96 : x - 38);
+                var commonPriorities = leftCompartment.Intersect(rightCompartment).Sum();
+                value += commonPriorities;
+            }
+            return value;
+
+        }
+    }
+}
--- UI/Program.cs
+++ UI/Program.cs
@@ -1,6 +1,6 @@
 ﻿using UI;
 
-var day2 = new Day2();
-var output = day2.RunDay2_1();
+var day3 = new Day3();
+var output = day3.RunPart1();
 
 Console.WriteLine(output);
